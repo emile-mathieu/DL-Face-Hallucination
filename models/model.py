fdd8997137f674978e667739362e61ff03df52aa
Emile Mathieu <emilepc2003@hotmail.com>
2026-03-06 17:31:27 +0800

    Add initial project structure with draft notebook, model, training, and testing scripts

---
+++ models/model.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class SimpleNN(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+
+        # To do
+
+    def forward(self):
+        # To do 
+        pass
