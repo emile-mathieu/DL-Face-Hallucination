07864f090ca178f01958d3cb2025b0b8b5ecfe51
Tan Shi Yu Julia <tanshiyujulia@gmail.com>
2026-03-28 22:09:55 +0800

    Modify evaluate function to accept dataset and denormalize
    Updated evaluate function to include dataset parameter and denormalize outputs.

--- training/test.py
+++ training/test.py
@@ -11,8 +11,7 @@ def psnr(pred, target):
     mse = torch.clamp(mse, min=1e-10)
     return 10 * torch.log10(1.0 / mse)
 
-
-def evaluate(model, dataloader, device):
+def evaluate(model, dataloader, device, dataset):
     model.to(device)
     model.eval()
 
@@ -22,23 +21,19 @@ def evaluate(model, dataloader, device):
     total_ssim = 0.0
     total_batches = 0
 
-    # reset metric
     ssim_metric.reset()
 
     with torch.no_grad():
-        # As a reminder = Iin: low-res input, IH: high-res target
         for batch_idx, (Iin, IH) in enumerate(dataloader):
             Iin = Iin.to(device).float()
             IH = IH.to(device).float()
 
-            # forward pass
             outputs = model(Iin)
 
-            # convert [-1,1] → [0,1]
-            pred = torch.tanh(outputs)
-            target = torch.tanh(IH)
+            #compare the denormalized output with the denormalized IH
+            pred = dataset.denormalize(outputs)
+            target = dataset.denormalize(IH)
 
-            # compute metrics
             batch_psnr = psnr(pred, target)
             batch_ssim = ssim_metric(pred, target)
 
@@ -46,7 +41,6 @@ def evaluate(model, dataloader, device):
             total_ssim += batch_ssim.item()
             total_batches += 1
 
-            # debug (only first batch)
             if batch_idx == 0:
                 print(f"Input (LR): {Iin.shape}")
                 print(f"Output (SR): {outputs.shape}")
@@ -57,7 +51,6 @@ def evaluate(model, dataloader, device):
 
     print(f"\nEvaluation Results → PSNR: {avg_psnr:.2f} dB | SSIM: {avg_ssim:.4f}")
 
-    # Save to CSV
     save_metrics_to_csv(
         "results/eval_metrics.csv",
         [avg_psnr, avg_ssim],
