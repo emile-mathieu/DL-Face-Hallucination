3c9e948c528d1b2bd89c7116df713af3077c4456
Tan Shi Yu Julia <tanshiyujulia@gmail.com>
2026-03-28 21:59:37 +0800

    Modify DataLoader functions to include mean and std
    Updated DataLoader functions to accept mean and std parameters for dataset normalization.

--- data/dataloader.py
+++ data/dataloader.py
@@ -4,34 +4,37 @@ from torch.utils.data import DataLoader
 
 # Preapre our DataLoaders for training, validation, and testing. 
 # Made different DataLoader settings (e.g., batch size, shuffling) for each training stage.
-def get_test_loader(data_path):
-    dataset = FaceDataset(image_dir=data_path)
+def get_test_loader(data_path, mean, std):
+    dataset = FaceDataset(image_dir=data_path, mean=mean, std=std)
 
     dataloader = DataLoader(
         dataset,
-        batch_size=1,   # one image at a time
+        batch_size=1, #one image at a time
         shuffle=False
     )
     return dataloader
 
-def get_val_loader(data_path, batch_size=32):
-    dataset = FaceDataset(image_dir=data_path)
+
+def get_val_loader(data_path, mean, std, batch_size=32):
+    dataset = FaceDataset(image_dir=data_path, mean=mean, std=std)
 
     dataloader = DataLoader(
         dataset,
         batch_size=batch_size,
-        shuffle=False,  # no shuffling for validation
+        shuffle=False, # no shuffling for validation
         pin_memory=True
     )
     return dataloader
 
-def get_dataloader(data_path, batch_size=32):
-    dataset = FaceDataset(image_dir=data_path)
+
+def get_dataloader(data_path, mean, std, batch_size=32):
+    dataset = FaceDataset(image_dir=data_path, mean=mean, std=std)
 
     dataloader = DataLoader(
         dataset,
         batch_size=batch_size,
-        shuffle=True,      
-        pin_memory=True  # This is for faster GPU transfer, allows tensors to be allocated in page-locked memory
+        shuffle=True,
+        pin_memory=True # This is for faster GPU transfer, allows tensors to be allocated in page-locked memory
     )
     return dataloader
+
